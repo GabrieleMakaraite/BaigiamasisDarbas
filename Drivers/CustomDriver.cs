9dc2596f5f91843759388df9b95714c481c534f9
GabrieleMakaraite <93566540+GabrieleMakaraite@users.noreply.github.com>
2021-11-22 11:29:31 +0200

    Add files via upload

---
+++ Drivers/CustomDriver.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaigiamasisDarbas.Drivers
+{
+    public class CustomDriver
+    {
+        public static IWebDriver GetChromeDriver()
+        {
+            return GetDriver(Browsers.Chrome);
+        }
+
+        private static IWebDriver GetDriver(Browsers browserName)
+        {
+            IWebDriver driver = null;
+
+            switch (browserName)
+            {
+                case Browsers.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            return driver;
+        }
+    }
+}
