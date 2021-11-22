a69c2396f3ac9599e9cc568d25eda88cdc20e828
GabrieleMakaraite <93566540+GabrieleMakaraite@users.noreply.github.com>
2021-11-22 11:31:06 +0200

    Add files via upload

---
+++ Test/BaseTest.cs
@@ -0,0 +1,56 @@
+﻿using BaigiamasisDarbas.Drivers;
+using BaigiamasisDarbas.Page;
+using BaigiamasisDarbas.Tools;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BaigiamasisDarbas.Test
+{
+    public class BaseTest
+    {
+        public static IWebDriver driver;
+
+        public static ProductsSortPage _productsSortPage;
+        public static JewelryAppraisalPage _jewelryAppraisalPage;
+        public static CartDropDownPage _cartDropDownPage;
+        public static CartPage _cartPage;
+        public static SearchPage _searchPage;
+        public static FreeGiftPage _freeGiftPage;
+        public static CountryPage _countryPage;
+        public static HomePage _homePage;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            driver = CustomDriver.GetChromeDriver();
+
+            _productsSortPage = new ProductsSortPage(driver);
+            _jewelryAppraisalPage = new JewelryAppraisalPage(driver);
+            _cartDropDownPage = new CartDropDownPage(driver);
+            _cartPage = new CartPage(driver);
+            _searchPage = new SearchPage(driver);
+            _freeGiftPage = new FreeGiftPage(driver);
+            _countryPage = new CountryPage(driver);
+            _homePage = new HomePage(driver);
+        }
+        [TearDown]
+        public static void TakeScreenshot()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+                MyScreenshot.MakeScreenshot(driver);
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Test/CharmedAromaTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaigiamasisDarbas.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BaigiamasisDarbas.Test
+{
+    public class CharmedAromaTest : BaseTest
+    {          
+        [Order(1)]
+        [Test]
+        public void TestPopUpCart()
+        {
+            _homePage.NavigateToDefaultPage().ChooseSize().AddToCart().VerifyProductResult("Candy Cane Lane Candle - Ring Collection").VerifySizeResult("Size 6");                   
+        }
+
+        [Order(2)]
+        [Test]
+        public void TestSort()
+        {
+            _homePage.NavigateToDefaultPage().ChooseCategory();
+            _productsSortPage.NavigateToDefaultPage().ClickDropDownButton().ChooseSortAlphabetically().VerifySortResult();
+        }
+
+        [Order(3)]
+        [TestCase("gabyte.mm@gmail.com","2GZZL7", "Appraised for $55", TestName = "Check jewelry code 2GZZL7")]
+        [TestCase("gabyte.mm@gmail.com","JKSRE5", "Appraised for $55", TestName = "Check jewelry code JKSRE5")]
+        public void TestJewelryAppraisal(string email, string code, string expectedResult)
+        {
+            _jewelryAppraisalPage.NavigateToDefaultPage().InsertEmail(email).InsertCode(code).ClickAppraisalButton().VerifyAppraisalResult(expectedResult);
+        }
+
+        [Order(4)]
+        [TestCase("Penguin Candle - 925 Sterling Silver Penguin Necklace Collection", TestName = "Search Penguin Candle")]
+        [TestCase("Penguin Bath Bomb - Blue Opal Ring Collection", TestName = "Search Penguin Bath Bomb")]
+        public void TestSearch(string product)
+        {
+            _searchPage.NavigateToDefaultPage().ClickSearchButton().InsertProductToSearchInput(product).ClickSubmitSearch().VerifySearchResults(product);
+        }
+
+        [Order(5)]
+        [Test]
+        public void TestGift()
+        {
+            _freeGiftPage.NavigateToDefaultPage().InsertQuantity("3").ClickAddToCartButton().VerifyResult();
+        }
+
+        [Order(6)]
+        [Test]
+        public void TestDropDown()
+        {
+            _cartDropDownPage.NavigateToDefaultPage().ChooseSize().AddToCartButtonClick().VerifyResult("Adjustable Ring").SelectFromDropDownByValue("Necklace").ClickCheckOutButton();
+            _cartPage.NavigateToDefaultPage().VerifyResult("Necklace");
+        }
+
+        [Order(7)]
+        [Test]
+        public void TestCountryDropDown()
+        {
+            _countryPage.NavigateToDefaultPage().ClickCountrySelector().ChooseCountry().ClosePopUp().VerifyResult();
+        }
+       
+        
+
+
+
+    }
+}
