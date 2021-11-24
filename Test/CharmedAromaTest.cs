2d8f450ab28965718c6da99b1bd1790cd8412d09
GabrieleMakaraite <93566540+GabrieleMakaraite@users.noreply.github.com>
2021-11-24 16:53:34 +0200

    Update CharmedAromaTest.cs

--- Test/CharmedAromaTest.cs
+++ Test/CharmedAromaTest.cs
@@ -13,14 +13,15 @@ namespace BaigiamasisDarbas.Test
 {
     public class CharmedAromaTest : BaseTest
     {          
-        [Order(1)]
+
         [Test]
         public void TestPopUpCart()
         {
-            _homePage.NavigateToDefaultPage().ChooseSize().AddToCart().VerifyProductResult("Candy Cane Lane Candle - Ring Collection").VerifySizeResult("Size 6");                  
+            _homePage.NavigateToDefaultPage().ChooseSize().AddToCart()
+                .VerifyProductResult("Candy Cane Lane Candle - Ring Collection")
+                .VerifySizeResult("Size 6");                  
         }
 
-        [Order(2)]
         [Test]
         public void TestSort()
         {
@@ -28,44 +29,44 @@ namespace BaigiamasisDarbas.Test
             _productsSortPage.NavigateToDefaultPage().ClickDropDownButton().ChooseSortAlphabetically().VerifySortResult();
         }
 
-        [Order(3)]
         [TestCase("gabyte.mm@gmail.com","2GZZL7", "Appraised for $55", TestName = "Check jewelry code 2GZZL7")]
         [TestCase("gabyte.mm@gmail.com","JKSRE5", "Appraised for $55", TestName = "Check jewelry code JKSRE5")]
         public void TestJewelryAppraisal(string email, string code, string expectedResult)
         {
-            _jewelryAppraisalPage.NavigateToDefaultPage().InsertEmail(email).InsertCode(code).ClickAppraisalButton().VerifyAppraisalResult(expectedResult);
+            _jewelryAppraisalPage.NavigateToDefaultPage().InsertEmail(email).InsertCode(code).ClickAppraisalButton()
+                .VerifyAppraisalResult(expectedResult);
         }
 
-        [Order(4)]
         [TestCase("Penguin Candle - 925 Sterling Silver Penguin Necklace Collection", TestName = "Search Penguin Candle")]
         [TestCase("Penguin Bath Bomb - Blue Opal Ring Collection", TestName = "Search Penguin Bath Bomb")]
         public void TestSearch(string product)
         {
-            _searchPage.NavigateToDefaultPage().ClickSearchButton().InsertProductToSearchInput(product).ClickSubmitSearch().VerifySearchResults(product);
+            _searchPage.NavigateToDefaultPage().ClickSearchButton().InsertProductToSearchInput(product).ClickSubmitSearch()
+                .VerifySearchResults(product);
         }
 
-        [Order(5)]
         [Test]
         public void TestQuantity()
         {
-            _quantityPage.NavigateToDefaultPage().InsertQuantity("2").ClickAddToCartButton().ClickCheckOutButton().VerifyResult("2");
+            _quantityPage.NavigateToDefaultPage().InsertQuantity("2").ClickAddToCartButton().ClickCheckOutButton()
+                .VerifyResult("2");
 
         }
 
-        [Order(6)]
-        [Test]
-        public void TestDropDown()
-        {
-            _cartDropDownPage.NavigateToDefaultPage().ChooseSize().AddToCartButtonClick().VerifyResult("Adjustable Ring").SelectFromDropDownByValue("Necklace").ClickCheckOutButton();
-            _cartPage.NavigateToDefaultPage().VerifyResult("Necklace");
-        }
-
-        [Order(7)]
         [Test]
         public void TestCountryDropDown()
         {
             _countryPage.NavigateToDefaultPage().ClickCountrySelector().ChooseCountry().ClosePopUp().VerifyResult();
         }
-             
+
+        [Test]
+        public void TestDropDown()
+        {
+            _cartDropDownPage.NavigateToDefaultPage().ChooseSize().AddToCartButtonClick()
+                .VerifyResult("Adjustable Ring").SelectFromDropDownByValue("Necklace")
+                .ClickCheckOutButton();
+            _cartPage.NavigateToDefaultPage().VerifyResult("Necklace");
+        }                  
     }
 }
+
