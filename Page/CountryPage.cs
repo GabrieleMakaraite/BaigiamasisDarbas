3bb00b375d9db7690b90d95555e73cf6f5b6bc70
GabrieleMakaraite <93566540+GabrieleMakaraite@users.noreply.github.com>
2021-12-11 19:50:31 +0200

    Update CountryPage.cs

--- Page/CountryPage.cs
+++ Page/CountryPage.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
@@ -15,8 +15,6 @@ namespace BaigiamasisDarbas.Page
         private IWebElement CountryButton => Driver.FindElement(By.CssSelector("#cart > li:nth-child(5) > div > div.flag-dropdown > div > div > div"));
         private IWebElement ResultTextElement => Driver.FindElement(By.XPath("/html/body/header/section[1]/div[1]/div/div[2]/div[1]/span"));
         private IWebElement CanadaButton => Driver.FindElement(By.CssSelector("#cart > li:nth-child(5) > div > div.flag-dropdown > ul > li:nth-child(2)"));
-        private IWebElement DetailsButton => Driver.FindElement(By.CssSelector("#epb_button"));
-        private IWebElement ShopNowButton => Driver.FindElement(By.CssSelector("#cboxLoadedContent > p:nth-child(12) > input"));
 
         public CountryPage(IWebDriver webdriver) : base(webdriver)
         {
@@ -38,13 +36,6 @@ namespace BaigiamasisDarbas.Page
         public CountryPage ChooseCountry()
         {
             CanadaButton.Click();
-            return this;
-        }
-
-        public CountryPage ClosePopUp()
-        {
-            DetailsButton.Click();
-            ShopNowButton.Click();
             return this;
         }
 
